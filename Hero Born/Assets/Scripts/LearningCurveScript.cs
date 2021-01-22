ce3ecdd196ccfc6959f28f734fd78e0c00eee97a
AiarCost <65986842+AiarCost@users.noreply.github.com>
2021-01-22 14:47:37 -0800

    Imported Unity Project

---
+++ Hero Born/Assets/Scripts/LearningCurveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurveScript : MonoBehaviour
+{
+
+    public int CurrentAge = 18;
+    public int AddedAge = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComputeAge();
+    }
+
+   /// <summary>
+   /// Computes a modified age integer
+   /// </summary>
+    void ComputeAge()
+    {
+        Debug.Log(CurrentAge + AddedAge);
+    }
+}
